74c011e5fc17b1fb8b25cf15da4e7e4a506d29f3
159753klapaucius159753 <juanp_jpr@hotmail.com>
2021-01-10 19:16:41 -0300

    estructura catalogo productos

--- front/ecom/src/app/visor-cat/visor-cat.component.ts
+++ front/ecom/src/app/visor-cat/visor-cat.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visor-cat.component.css']
 })
 export class VisorCatComponent implements OnInit {
-
-  constructor() { }
+  users: Array<number> = [1, 2, 3,4,5];
+  constructor() {
+   
+    console.log("declarado")
+   }
 
   ngOnInit(): void {
+   
+    console.log(this.users.length)
   }
 
 }
