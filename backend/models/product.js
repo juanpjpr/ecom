22955970668d0822edfab727933c5750beb9abdc
159753klapaucius159753 <juanp_jpr@hotmail.com>
2021-02-07 21:43:14 -0300

    agrege mongo, y intento de swagger

--- backend/models/product.js
+++ backend/models/product.js
@@ -6,6 +6,10 @@ const ProductSchema=new Schema({
     descripcion: {type: String, required: true},
     talle: {type: String},
     stock: {type: String},
-})
+},{
+
+    versionKey: false // You should be aware of the outcome after set to false
+
+});
 
 module.exports=mongoose.model('Product',ProductSchema)
--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,57 @@ const app = express()
   .use(cors())
   .use(bodyParser.json())
 
+  const swaggerJsDoc=require('swagger-jsdoc')
+  const swaggerUi= require('swagger-ui-express')
+  const swaggerOptions= {
+    swaggerDefinition:{
+      info:{
+        title:"Inventario",
+        description:"API para control de stock",
+        contact:{
+          name:"Juan Pablo Repik"
+        },
+        servers:["http://localhost:3000/"]
+     }},
+     apis:["server.js"]
+  }
+
+  const swaggerDocs=swaggerJsDoc(swaggerOptions)
+  app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs))
+
+  
+
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
 
-app.get('/products',(req,res)=>{
-  // const allProducts = await Product.find()
-  //console.log(allProduct)
-  res.send("hola")
+app.get('/all',async (req,res)=>{
+  const allProducts = await Product.find()
+  
+  res.send(JSON.stringify(allProducts))
+})
+
+app.post('/add',async (req,res)=>{
+  const{descripcion,talle,stock}=req.body
+  const newProd= new Product({descripcion,talle,stock})
+  await newProd.save()
+  console.log(newProd)
+  res.send(JSON.stringify(newProd))
+})
+
+app.get('/f/descripcion/:prod',async(req,res)=>{
+  const result=await Product.find({"descripcion":req.params.prod})
+  if (result.length==0)
+  res.send("-1")
+  else
+  res.send(JSON.stringify(result))
+})
+
+app.get('/d/descripcion/:prod',async(req,res)=>{
+  const result=await Product.find({"descripcion":req.params.prod}).remove()
+  if (result.length==0)
+  res.send("-1")
+  else
+  res.send(JSON.stringify(result))
 })
